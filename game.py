dfe874f58657b1a99d701bf49803bf3eb358cd82
athenanoggle <akn42@georgetown.edu>
2021-09-21 13:35:22 -0400

    Update game.py

--- game.py
+++ game.py
@@ -1,14 +1,42 @@
 import random 
 
-print("Rock, Paper, Scissors, Shoot!") 
+print("Welcome 'player one' to Rock-Paper-Scissors!")
 
 #Prompt user for input
-user_choice = input ("Choose 'rock' or 'paper' or 'scissors.': ")
+user_choice = input ("Choose 'rock,' 'paper,' or 'scissors': ")
+print ("User Choice:")
 print(user_choice)
 
+options = ["rock", "paper", "scissors"]
+
+#User choice validation
+if user_choice not in options:
+    print ("Invalid choice. I am sorry. The game will now exit.")
+    exit()
+
 #Computer choice (at random)
-
-options= ["rock", "paper", "scissors"]
-
 computer_choice = random.choice(options)
+print ("Computer Choice:")
 print(computer_choice)
+
+#Display of winner
+if user_choice == computer_choice:
+    print ("Both players selected the same. It's a tie.")
+elif user_choice == "rock":
+    if computer_choice == "scissors":
+        print ("Rock smashes scissors. You win.")
+    else: 
+        print("Paper covers rock. You lose.")
+elif user_choice == "paper":
+    if computer_choice == "rock":
+        print("Paper covers rock. You win.")
+    else:
+        print("Scissors cut paper. You lose.")
+elif user_choice == "scissors":
+    if computer_choice == "paper":
+        print("Scissors cut paper. You win.")
+    else:
+        print("Rock smashes scissors. You lose.")
+
+print ("Thanks for playing. Play again!")
+
